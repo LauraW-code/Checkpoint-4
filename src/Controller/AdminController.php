f2786ebd1242d9819e4889f5df4fc8a82974c50b
LauraW-code <laura.wolff@live.fr>
2024-02-01 10:52:23 +0100

    crud character + dashboard admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,10 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Repository\CharacterRepository;
 use App\Entity\Character;
 use App\Entity\Backrgound;
 use App\Entity\Effect;
 use App\Entity\Contact;
+use App\Form\CharacterType;
 
 #[Route('/admin', name:'admin_')]
 class AdminController extends AbstractController
@@ -17,5 +23,52 @@ class AdminController extends AbstractController
     public function indexDashboard(): Response
     {
         return $this->render('admin/dashboard.html.twig');
+    }
+
+    #[Route('/characters', name:'characters')]
+    public function showCharacters(
+        CharacterRepository $characterRepository
+    ): Response {
+        $characters = $characterRepository->findAll();
+
+        return $this->render('admin/show_characters.html.twig', ['characters' => $characters]);
+    }
+
+    #[Route('/editCharacter/{id}', name: 'edit_character')]
+    public function editCharacter(
+        Request $request,
+        Character $character,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $form = $this->createForm(CharacterType::class, $character);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'The character has been edited successfully');
+
+            return $this->redirectToRoute('admin_characters');
+        }
+
+
+        return $this->render('admin/edit_character.html.twig', [
+            'form' => $form->createView(),
+            'character' => $character]);
+    }
+
+    #[Route('deleteCharacter/{id}', name: 'delete_character')]
+    public function deleteCharacter(
+        Request $request,
+        Character $character,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $entityManager->remove($character);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'This character has been successfully removed');
+
+        return $this->redirectToRoute('admin_characters');
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Character;
+use App\Repository\CharacterRepository;
 
 class HomeController extends AbstractController
 {
@@ -12,5 +14,14 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/downloads', name:'app_downloads')]
+    public function downloadPage(
+        CharacterRepository $characterRepository
+    ): Response {
+        $characters = $characterRepository->findAll();
+
+        return $this->render('home/downloads.html.twig', ['characters' => $characters]);
     }
 }
